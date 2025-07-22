2c743863f70c0635f023fe5ea8c126193bcb50d9
Chathurajk <mchathurajk95@gmail.com>
2025-07-22 22:24:48 +0530

    using useState hook🏎

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+import HomePage from './pages/homePage';
+import LoginPage from './pages/loginPage';
+import RegisterPage from './pages/registerPage';
+import TestPage from './pages/testPage';
+
+
+
 
 function App() {
-  
-
   return (
-    <>
-      <div>
+    <BrowserRouter>
+      <div className="w-full h-screen flex justify-center items-center">
+        
+        	<Routes path="/">
+						<Route path="/" element={<HomePage/>}/>
+						<Route path="/login" element={<LoginPage/>}/>
+						<Route path="/test" element={<TestPage/>}/>
+						<Route path="/register" element={<RegisterPage/>}/>
+            <Route path="/admin/*" element={<AdminPag/>}/>
+						
+					</Routes>
         
       </div>
       
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
